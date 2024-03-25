9d2c0f86beb85da69ce6fd63bbc522ffc6f52ea3
Ameer Hamza <ah18r@fsu.edu>
2024-03-25 18:41:37 -0400

    Added a definition for constraint for offset domain
    Signed-off-by: Ameer Hamza <ah18r@fsu.edu>

---
+++ src/crab/constraint.cpp
@@ -0,0 +1,50 @@
+
+#include "constraint.hpp"
+
+namespace crab {
+
+std::ostream &operator<<(std::ostream &o, const constraint_t &c) {
+    c.write(o);
+    return o;
+}
+
+void constraint_t::simplify(std::shared_ptr<slacks_t> slacks) {
+    auto neg = _expression_rhs.negate();
+    _expression_lhs = _expression_lhs + _expression_rhs.negate();
+    _expression_rhs = expression_t(0);
+    for (auto &e : _expression_lhs.get_symbol_terms()) {
+        if (e.second == 0) {
+            _expression_lhs -= e.first;
+        }
+        else if (e.first.is_slack()) {
+            auto val = slacks->find(e.first);
+            if (val != slacks->end()) {
+                _expression_lhs -= e.first;
+                _expression_lhs = _expression_lhs +
+                    val->second.to_interval() * interval_t{(int)e.second};
+            }
+        }
+    }
+}
+
+bool constraint_t::is_bottom(std::shared_ptr<slacks_t> slacks) {
+    simplify(slacks);
+    if (!_expression_lhs.is_constant()) {
+        return false;
+    }
+    auto constant = _expression_lhs.get_interval();
+    //auto interval_0 = _expression_rhs.get_interval();
+    return (bound_t{number_t{0}} < constant.lb());
+    //return (constant.ub() > interval_0.lb()) && ((constant & interval_0) == interval_t::bottom());
+}
+
+constraint_t constraint_t::negate() const {
+    // neg(x <= y) -> x > y -> y < x -> y <= x - 1
+    return constraint_t(_expression_rhs, _expression_lhs + (-1));
+}
+
+void constraint_t::write(std::ostream &o) const {
+    o << _expression_lhs << " <= " << _expression_rhs;
+}
+
+} // namespace crab
---
+++ src/crab/constraint.hpp
@@ -0,0 +1,42 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "expression.hpp"
+#include "common.hpp"
+
+namespace crab {
+
+using slacks_t = std::map<symbol_t, mock_interval_t>;
+
+// represents a constraint of the form lhs <= rhs
+class constraint_t {
+  private:
+    expression_t _expression_lhs;
+    expression_t _expression_rhs;
+
+  public:
+    constraint_t(expression_t lhs, expression_t rhs)
+        : _expression_lhs(lhs), _expression_rhs(rhs) {}
+
+    bool is_bottom(std::shared_ptr<slacks_t>);
+
+    expression_t get_lhs() const { return _expression_lhs; }
+    expression_t get_rhs() const { return _expression_rhs; }
+
+    constraint_t operator+(const constraint_t&) const;    
+    constraint_t operator+(int) const;
+    constraint_t operator+(interval_t) const;
+    constraint_t operator-(const constraint_t&) const;
+    constraint_t operator|(const constraint_t&) const;
+    constraint_t operator<=(const constraint_t&) const;
+    constraint_t operator>(const constraint_t&) const;
+    bool is_equality() const;
+    void simplify(std::shared_ptr<slacks_t>);
+    [[nodiscard]] constraint_t negate() const;
+    void write(std::ostream&) const;
+};
+
+std::ostream& operator<<(std::ostream &, const constraint_t&);
+
+} // namespace crab
