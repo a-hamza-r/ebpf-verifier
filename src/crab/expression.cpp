e5096fa203842d87805fad14b11b47214f78bc64
Ameer Hamza <ah18r@fsu.edu>
2024-03-25 18:40:51 -0400

    Added a definition for expression for offset domain
    Signed-off-by: Ameer Hamza <ah18r@fsu.edu>

---
+++ src/crab/expression.cpp
@@ -0,0 +1,132 @@
+
+#include "expression.hpp"
+
+namespace crab {
+
+std::ostream& operator<<(std::ostream& o, const expression_t& e) {
+    e.write(o);
+    return o;
+}
+
+bool expression_t::operator==(const expression_t &other) const {
+    return _symbol_terms == other._symbol_terms && _interval == other._interval;
+}
+
+bool expression_t::operator<(const expression_t &other) const {
+    return _symbol_terms == other._symbol_terms && _interval.ub() < other._interval.lb();
+}
+
+bool expression_t::operator<=(const expression_t &other) const {
+    return _symbol_terms == other._symbol_terms && _interval.ub() <= other._interval.lb();
+}
+
+bool expression_t::operator>(const expression_t &other) const {
+    return _symbol_terms == other._symbol_terms && _interval.lb() > other._interval.ub();
+}
+
+bool expression_t::is_singleton() const {
+    return _symbol_terms.size() == 1 && _interval == interval_t{0};
+}
+
+bool expression_t::contains(const symbol_t &s) const {
+    return _symbol_terms.find(s) != _symbol_terms.end();
+}
+
+symbol_t expression_t::get_singleton() const {
+    if (!is_singleton()) {
+        throw std::runtime_error("expression does not contain a single symbol");
+    }
+    return _symbol_terms.begin()->first;
+}
+
+static void insert(symbol_terms_t &terms, const std::pair<symbol_t, int8_t>& kv) {
+    if (terms.find(kv.first) != terms.end()) {
+        terms[kv.first] += kv.second;
+    } else {
+        terms[kv.first] = kv.second;
+    }
+}
+
+expression_t expression_t::substitute(const symbol_t &from, const expression_t &to) const {
+    expression_t result = *this;
+    auto it = result._symbol_terms.find(from);
+    if (it != result._symbol_terms.end()) {
+        result._symbol_terms.erase(it);
+        result = result + to;
+    }
+    return result;
+}
+
+expression_t expression_t::negate() const {
+    symbol_terms_t new_terms;
+    for (const auto &term : _symbol_terms) {
+        new_terms[term.first] = -term.second;
+    }
+    return expression_t(new_terms, -_interval);
+}
+
+expression_t expression_t::operator+(const expression_t &other) const {
+    auto new_terms = _symbol_terms;
+    for (const auto &term : other._symbol_terms) {
+        insert(new_terms, term);
+    }
+    return expression_t(new_terms, _interval + other._interval);
+}
+
+expression_t expression_t::operator+(interval_t interval) const {
+    return expression_t(_symbol_terms, _interval + interval);
+}
+
+expression_t expression_t::operator+(int n) const {
+    return operator+(interval_t{n});
+}
+
+expression_t expression_t::operator|(const expression_t &other) const {
+    if (_symbol_terms.size() != other._symbol_terms.size()) {
+        return expression_t();
+    }
+    symbol_terms_t new_terms;
+    for (auto &term : _symbol_terms) {
+        auto it = other._symbol_terms.find(term.first);
+        if (it == other._symbol_terms.end()) {
+            // we need to know what is the other slack, but it's not accesible directly
+            //if (term.first.is_slack() && it->first.is_slack()) {
+                auto new_slack = symbol_t::make();
+                new_terms[new_slack] = 1;
+            //}
+            //else {
+            //    return expression_t();
+            //}
+        }
+        else {
+            // TODO: some complex logic is needed here
+            new_terms[term.first] = term.second;
+        }
+    }
+    interval_t new_interval = _interval | other._interval;
+    return expression_t(new_terms, new_interval);
+}
+
+void expression_t::write(std::ostream &o) const {
+    size_t i = 0;
+    for (const auto &term : _symbol_terms) {
+        if (term.second != 1) {
+            o << (int)term.second << " * ";
+        }
+        o << term.first;
+        if (i < _symbol_terms.size() - 1) {
+            o << " + ";
+        }
+        i++;
+    }
+    if (auto s = _interval.singleton()) {
+        if ((int)*s != 0 || _symbol_terms.empty()) {
+            if (!_symbol_terms.empty()) o << " + ";
+            o <<  *s;
+        }
+    } else {
+        o << " + " << _interval;
+    }
+}
+
+}  // namespace crab
---
+++ src/crab/expression.hpp
@@ -0,0 +1,78 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <map>
+#include <cassert>
+#include "symbol.hpp"
+#include "interval.hpp"
+
+namespace crab {
+// An expression is of form: Ax + By + Cz + ... + I.
+// x, y, z, ... are symbols, and A, B, C, ... are coefficients.
+// I is an interval.
+    
+using symbol_terms_t = std::map<symbol_t, int8_t>;
+class expression_t {
+  private:
+    symbol_terms_t _symbol_terms;
+    interval_t _interval;
+
+  public:
+    expression_t() : _interval(interval_t::top()) {};
+    expression_t(symbol_terms_t symbol_terms, interval_t interval = interval_t{0})
+        : _symbol_terms(symbol_terms), _interval(interval) {}
+    expression_t(symbol_t symbol) : _interval(interval_t{0}) {
+        _symbol_terms[symbol] = 1;
+    }
+    expression_t(interval_t interval)
+        : _symbol_terms(symbol_terms_t{}), _interval(interval) {}
+    expression_t(int n)
+        : _symbol_terms(symbol_terms_t{}), _interval(interval_t{n}) {}
+    bool operator==(const expression_t &other) const;
+    bool operator<(const expression_t &other) const;
+    bool operator<=(const expression_t &other) const;
+    bool operator>(const expression_t &other) const;
+    void operator-=(const symbol_t& s) { _symbol_terms.erase(s); }
+    bool is_constant() const { return _symbol_terms.empty(); }
+    expression_t negate() const;
+    expression_t operator+(const expression_t &other) const;
+    expression_t operator+(interval_t) const;
+    expression_t operator+(int n) const;
+    expression_t operator|(const expression_t &other) const;
+    void write(std::ostream &o) const;
+    symbol_terms_t get_symbol_terms() const { return _symbol_terms; }
+    bool is_singleton() const;
+    expression_t substitute(const symbol_t &, const expression_t &) const;
+    bool contains(const symbol_t &s) const;
+    symbol_t get_singleton() const;
+    interval_t get_interval() const { return _interval; }
+
+    static expression_t begin() {
+        return expression_t({std::make_pair(symbol_t::begin(), 1)});
+    }
+
+    static expression_t end() {
+        return expression_t({std::make_pair(symbol_t::end(), 1)});
+    }
+
+    static expression_t meta() {
+        return expression_t({std::make_pair(symbol_t::meta(), 1)});
+    }
+
+    static expression_t nu() {
+        return expression_t({std::make_pair(symbol_t::nu(), 1)});
+    }
+
+    static expression_t pkt_symbol() {
+        return expression_t({std::make_pair(symbol_t::pkt_symbol(), 1)});
+    }
+
+    static expression_t make_slack() {
+        return expression_t({std::make_pair(symbol_t::make(), 1)});
+    }
+};
+
+std::ostream& operator<<(std::ostream &, const expression_t&);
+
+}  // namespace crab
