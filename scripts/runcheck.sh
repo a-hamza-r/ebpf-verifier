e6ea9803a58aab1912a0ebf687072e05811b3d6b
Ameer Hamza <ah18r@fsu.edu>
2024-11-19 10:03:46 -0500

    Updated all benchmarks script
    Signed-off-by: Ameer Hamza <ah18r@fsu.edu>

--- scripts/runcheck.sh
+++ scripts/runcheck.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ### Update this path to the root of your prevail repository.
-PREVAIL_ROOT=/Users/jorge/Repos/prevail-type-proofs
+PREVAIL_ROOT=$(pwd)
 
 
 EBPF_BENCHMARKS=${PREVAIL_ROOT}/ebpf-samples
 PREVAIL_CHECK=${PREVAIL_ROOT}/check
-DOMAINS="zoneCrab"
+DOMAINS="inference"
 PREFIX=prevail_$(date +"%m%d%y%H%M")
 
 for dom in $DOMAINS
@@ -16,15 +16,17 @@ do
     echo "File,Result,Cpu,Mem"  1>> ${PREFIX}_${dom}.csv
     for f in ${EBPF_BENCHMARKS}/*/*.o
     do
-	sections=($(${PREVAIL_CHECK} $f -l 2> /dev/null))
-	for s in "${sections[@]}"
-	do
-	    echo "${PREVAIL_CHECK} ${f} ${s} --domain=${dom}" >> log_${dom}.txt
-	    echo -n $f:$s 1>> ${PREFIX}_${dom}.csv
-	    o=$(${PREVAIL_CHECK} ${f} ${s} --domain=${dom} 2>>log_${dom}.txt)
-	    echo -n ",$o" 1>> ${PREFIX}_${dom}.csv
-	    echo 1>> ${PREFIX}_${dom}.csv
-	done
+      ${PREVAIL_CHECK} $f -l 2> /dev/null | while read -r s; do
+      if [ -n "$s" ]; then
+        values=$(echo "$s" | awk -F' |=' '{print $2, $4}')
+        read section function <<< "$values"
+        echo "${PREVAIL_CHECK} ${f} ${section} ${function} --domain=${dom}" >> log_${dom}.txt
+        echo -n $f:$section:$function 1>> ${PREFIX}_${dom}.csv
+        o=$(${PREVAIL_CHECK} ${f} ${section} ${function} --domain=${dom} 2>>log_${dom}.txt)
+        echo -n ",$o" 1>> ${PREFIX}_${dom}.csv
+        echo 1>> ${PREFIX}_${dom}.csv
+      fi
+		done
     done
     echo "DONE"
 done
